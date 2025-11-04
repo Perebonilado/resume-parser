5cb2270f3e50ebbc6ba34750210ecc513a2271a6
perebonilado <eradiribonnie@gmail.com>
2025-11-04 14:43:44 +0100

    opacity bg

--- src/@shared/components/AppLoader.tsx
+++ src/@shared/components/AppLoader.tsx
@@ -6,15 +6,15 @@ interface Props {
   spinnerSize?: "sm" | "md";
 }
 
-export const AppLoader: FC<Props> = ({ loaderMessage, spinnerSize="md" }) => {
+export const AppLoader: FC<Props> = ({ loaderMessage, spinnerSize = "md" }) => {
   return (
-    <div className="flex flex-col gap-4 justify-center items-center h-screen fixed z-[8000] top-0 left-0 w-screen bg-black bg-opacity-80">
-      <Spinner size={spinnerSize}/>
+    <div className="flex flex-col gap-4 justify-center items-center h-screen fixed z-[8000] top-0 left-0 w-screen bg-black/60 backdrop-blur-sm">
+      <Spinner size={spinnerSize} />
       {loaderMessage && (
-        <p className="w-full max-w-[500px] px-3 mx-auto text-center max-md:text-sm font-semibold text-white">
+        <p className="w-full max-w-[500px] px-3 mx-auto text-center max-md:text-sm font-semibold text-white drop-shadow-lg">
           {loaderMessage}
         </p>
       )}
     </div>
   );
-};
+}
--- src/contexts/LoaderContext.tsx
+++ src/contexts/LoaderContext.tsx
@@ -13,21 +13,6 @@ const LoadingProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
   const { loading, loadingMessage } = useSelector(
     (state: RootState) => state.loaderReducer
   );
-  const { data: session } = useSession();
-  const dispatch = useDispatch();
-
-  // const fetchUserInfo = async () => {
-  //   const userRepository: UserRepository = new UserRepositoryImpl();
-  //   if (!session || !session?.user || !session?.user?.email) return;
-  //   const userData = await userRepository.findByEmail(session?.user?.email);
-  //   if (userData) {
-  //     dispatch(setUser(userData));
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchUserInfo();
-  // }, []);
 
   return (
     <LoaderContext.Provider value={{}}>
