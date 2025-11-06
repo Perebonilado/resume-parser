6b1c3680576bc63d25f358d2beb93fb701244e70
perebonilado <eradiribonnie@gmail.com>
2025-11-06 04:05:41 +0100

    head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,20 +4,26 @@ import ModalProvider from "@/contexts/ModalContext";
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Provider store={reduxStore}>
-        <ModalProvider>
-          <LoadingProvider>
-            <Component {...pageProps} />
-          </LoadingProvider>
-        </ModalProvider>
-        <ToastContainer />
-      </Provider>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Resume Extractor</title>
+      </Head>
+      <SessionProvider session={pageProps.session}>
+        <Provider store={reduxStore}>
+          <ModalProvider>
+            <LoadingProvider>
+              <Component {...pageProps} />
+            </LoadingProvider>
+          </ModalProvider>
+          <ToastContainer />
+        </Provider>
+      </SessionProvider>
+    </>
   );
 }
