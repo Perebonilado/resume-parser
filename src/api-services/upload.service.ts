bc881d43fd90a06e23563cb9a158a5f91ebfd05f
perebonilado <eradiribonnie@gmail.com>
2025-11-06 04:15:05 +0100

    invalidate tags

--- src/api-services/upload.service.ts
+++ src/api-services/upload.service.ts
@@ -3,6 +3,8 @@ import { API_BASE_URL } from "@/constants";
 import { secondsToMilliSeconds } from "@/lib";
 import { UploadDto } from "@/dto/UploadDto";
 import { ResumeUploadModel } from "@/business/ResumeUploadHandler";
+import { ResumeService } from "./resume.service";
+import { ResumeHistoryService } from "./resume-history.service";
 
 export const UploadService = createApi({
   reducerPath: "upload-api",
@@ -17,6 +19,11 @@ export const UploadService = createApi({
         body,
         method: "POST",
       }),
+      onQueryStarted: async (_, { queryFulfilled }) => {
+        await queryFulfilled;
+        ResumeService.util.invalidateTags(["resume"]);
+        ResumeHistoryService.util.invalidateTags(["resume-history"]);
+      },
     }),
   }),
 });
